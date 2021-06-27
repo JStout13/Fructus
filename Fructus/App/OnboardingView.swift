808c7cbfb6b30eeca39d74cd6ea6657dffa31fae
Jason Stout <30294372+HangoverSamui@users.noreply.github.com>
2021-06-26 21:27:52 -0500

    UploadProject

---
+++ Fructus/App/OnboardingView.swift
@@ -0,0 +1,28 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by Jason Stout on 6/24/21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var fruits: [Fruit] = fruitsData
+    
+    var body: some View {
+        TabView {
+            ForEach(fruits[0...5]) { item in
+                FruitCardView(fruit: item)
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView(fruits: fruitsData)
+    }
+}
